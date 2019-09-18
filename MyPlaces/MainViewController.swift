b6f366669f69b44df32d21f173e085637b01ebf8
delfi526 <delfi526@mail.ru>
2019-09-18 07:53:59 -0700

    Initial appear icon and launch screen Row Hight

---
+++ MyPlaces/MainViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MainViewController.swift
+//  MyPlaces
+//
+//  Created by delfi526 on 09/09/2019.
+//  Copyright © 2019 delfi526. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let placesName = ["Burger King","Lincoln","McDonalds","Terma Loundg","Silver Panda","Морковь","Июнь"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placesName.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = placesName[indexPath.row]
+        cell.imageView?.image = UIImage(named: placesName[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
+        return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 65
+    }
+    
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
