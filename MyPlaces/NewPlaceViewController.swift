220ed6aa28afd19721539cac1d7cd0159aa61b68
delfi526 <delfi526@mail.ru>
2019-09-30 10:30:05 -0700

    Create actionSheet on tap image Create UIimage Picker in actionSheet

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController {
 
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.tableFooterView = UIView() // Убрать разлиновку пустых строк в низу Table
@@ -20,6 +22,31 @@ class NewPlaceViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
             
+            let cameraIcon = #imageLiteral(resourceName: "camera")
+            let photoIcon = #imageLiteral(resourceName: "photo")
+            
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            camera.setValue(cameraIcon, forKey: "image") // вставляем значение по ключу
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            photo.setValue(photoIcon, forKey: "image") // вставляем значение по ключу
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
+            
         } else {
             view.endEditing(true)
         }
@@ -29,10 +56,36 @@ class NewPlaceViewController: UITableViewController {
 }
 // MARK: Text field delegate
 
-extension NewPlaceViewController: UITextFieldDelegate{
+extension NewPlaceViewController: UITextFieldDelegate, UINavigationControllerDelegate{
     // Скрываю клавиатуру по нажатию Done
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
+}
+
+// MARK: Work with image
+extension NewPlaceViewController : UIImagePickerControllerDelegate{
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController() // Экзепляр
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true // Позволяем масштабировать изображение
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill // масштабирование изображение по содержимому UIImage
+        imageOfPlace.clipsToBounds = true // Обрезка по границе UIImage
+        
+        dismiss(animated: true) // закрываем пикер
+        
+    }
+    
 }
